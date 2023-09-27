e2c239de68eb3c39a975bf8b183a0dcaf8e3a80b
jeniil <20it125@charusat.edu.in>
2023-09-27 16:45:38 +0530

    user_admin_pannel-updateUser

--- src/UserUI/main.userUI.js
+++ src/UserUI/main.userUI.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListUser from './components/Listuser.jsx'
 import AddUser from './components/Adduser.jsx'
-
+import UpdateUser from './components/Updateuser.jsx'
 function MainUserUI () {
     return (
         <BrowserRouter>
@@ -11,6 +11,7 @@ function MainUserUI () {
             <Routes>
                 <Route path="/" element={<ListUser />}/>
                 <Route path="/adduser" element={<AddUser />}/>
+                <Route path="/updateuser" element={<UpdateUser />}/>
             </Routes>
         </BrowserRouter>
     )
